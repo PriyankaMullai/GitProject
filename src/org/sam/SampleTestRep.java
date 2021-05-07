6b3011be8cce6e2f5e735e09214190ba83bd8856
PriyankaMullai <nanthinimppm@gmail.com>
2021-05-07 01:18:40 -0700

    working

---
+++ src/org/sam/SampleTestRep.java
@@ -0,0 +1,15 @@
+package org.sam;
+
+public class SampleTestRep {
+
+	private void sample1() {
+System.out.println("test1");
+	}
+	private void sample2() {
+System.out.println("test2");
+	}
+	private void sample3() {
+System.out.println("test3");
+	}
+	
+}
